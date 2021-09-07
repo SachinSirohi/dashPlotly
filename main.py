f37f419bb4dba1c7c80289502ea31f5c7a004af8
Sachin <er.sachinsirohi@gmail.com>
2021-09-07 23:37:06 +0530

    Graph2 Generated. using same formatting as Graph 1

--- main.py
+++ main.py
@@ -39,3 +39,39 @@ fig1.update_xaxes(type='category', showline=True, linewidth=0.5,
                   linecolor='gray', mirror=True)
 fig1.layout.plot_bgcolor = '#fff'
 fig1.layout.paper_bgcolor = '#fff'
+
+# fig1.show()
+
+# Graph2
+
+fig2 = px.bar(
+    chart2,
+    x='variety',
+    y='yield',
+    color='site',
+    width=800,
+    height=800,
+    )
+fig2.update_yaxes(
+    range=[0, 500],
+    title='Sum of yield',
+    showline=True,
+    linewidth=0.5,
+    linecolor='gray',
+    showgrid=True,
+    gridwidth=0.5,
+    gridcolor='grey',
+    mirror=True,
+    )
+fig2.update_xaxes(
+    type='category',
+    showline=True,
+    linewidth=0.5,
+    linecolor='gray',
+    tickangle=90,
+    mirror=True,
+    )
+fig2.layout.plot_bgcolor = '#fff'
+fig2.layout.paper_bgcolor = '#fff'
+
+# fig2.show()
