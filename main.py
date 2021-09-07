43c48c25d15029fb4a8e30589864cfca722d5e0b
Sachin <er.sachinsirohi@gmail.com>
2021-09-07 23:10:59 +0530

    Data Extracted

---
+++ main.py
@@ -0,0 +1,12 @@
+import altair as alt
+from vega_datasets import data
+import plotly.express as px
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+
+# Data import
+
+barley = data.barley()
+chart1 = barley.groupby(['site', 'year'])[['yield']].median().reset_index()
+chart2 = barley.groupby(['site', 'variety'])[['yield']].sum().reset_index()
